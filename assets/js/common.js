9d80106d1ce1da2af338f742efd3d5651b700b1c
root <master@neoaria.io>
2025-10-24 09:23:09 +0900

    slide 깜빡임 오류

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,25 +1,53 @@
 'use strict';
 
-
-
 $(document).ready(function() {
+    // Offcanvas를 애니메이션 없이 먼저 열기
+    initOffcanvas();
+
+    // 화면 높이 적용 함수
     function applyScreenHeight() {
         const viewportHeight = $(window).height();
         $('main, .slick-slide, .cover-slide .item').height(viewportHeight);
     }
+
     applyScreenHeight();
 
     $(window).resize(function() {
         applyScreenHeight();
     });
 
+    // 슬라이드 초기화 (부드러운 fade 효과)
     $('.cover-slide').slick({
         dots: true,
         infinite: true,
-        speed: 500,
-        fade: true,
+        speed: 1500,  // fade 전환 속도를 더 느리게 (1.5초)
+        fade: true,  // fade 효과 사용
         autoplay: true,
-        cssEase: 'linear'
+        autoplaySpeed: 300,  // 이미지 표시 시간 (5초)
+        cssEase: 'ease',  // 기본 easing
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        waitForAnimate: true  // 애니메이션이 완료될 때까지 대기
     });
-
 });
+
+/**
+ * Offcanvas 초기화 함수
+ * 페이지 로딩 시 offcanvas를 애니메이션 없이 열린 상태로 표시합니다
+ * 이후 사용자가 열고 닫을 때는 애니메이션이 적용됩니다
+ */
+function initOffcanvas() {
+    const offcanvasElement = $('#offcanvasGnb');
+    if (offcanvasElement.length) {
+        // 애니메이션 제거를 위한 클래스 추가
+        offcanvasElement.addClass('no-transition');
+
+        // 애니메이션 없이 바로 show 클래스 추가
+        offcanvasElement.addClass('show');
+
+        // 다음 프레임에서 no-transition 클래스 제거 (이후 애니메이션 활성화)
+        setTimeout(function() {
+            offcanvasElement.removeClass('no-transition');
+        }, 50);
+    }
+}
