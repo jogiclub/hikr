88adc93a2ffc39bb6f16941aaf998416513ec19b
root <master@neoaria.io>
2025-10-24 09:42:59 +0900

    slide 깜빡임 오류

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,10 @@
+// 파일 위치: assets/js/common.js
+// 역할: 화면 크기에 따라 Offcanvas 표시 여부를 동적으로 제어하는 함수
+
 'use strict';
 
 $(document).ready(function() {
-    // Offcanvas를 애니메이션 없이 먼저 열기
+    // Offcanvas를 애니메이션 없이 먼저 열기 (1200px 이상에서만)
     initOffcanvas();
 
     // 화면 높이 적용 함수
@@ -14,6 +17,7 @@ $(document).ready(function() {
 
     $(window).resize(function() {
         applyScreenHeight();
+        handleOffcanvasResize();
     });
 
     // 슬라이드 초기화 (부드러운 fade 효과)
@@ -33,21 +37,53 @@ $(document).ready(function() {
 
 /**
  * Offcanvas 초기화 함수
- * 페이지 로딩 시 offcanvas를 애니메이션 없이 열린 상태로 표시합니다
+ * 1200px 이상 화면에서만 페이지 로딩 시 offcanvas를 애니메이션 없이 열린 상태로 표시합니다
  * 이후 사용자가 열고 닫을 때는 애니메이션이 적용됩니다
  */
 function initOffcanvas() {
     const offcanvasElement = $('#offcanvasGnb');
+
     if (offcanvasElement.length) {
-        // 애니메이션 제거를 위한 클래스 추가
-        offcanvasElement.addClass('no-transition');
+        // 화면 크기가 1200px 이상인 경우에만 offcanvas를 자동으로 열기
+        if ($(window).width() >= 1200) {
+            // 애니메이션 제거를 위한 클래스 추가
+            offcanvasElement.addClass('no-transition');
 
-        // 애니메이션 없이 바로 show 클래스 추가
-        offcanvasElement.addClass('show');
+            // 애니메이션 없이 바로 show 클래스 추가
+            offcanvasElement.addClass('show');
 
-        // 다음 프레임에서 no-transition 클래스 제거 (이후 애니메이션 활성화)
-        setTimeout(function() {
-            offcanvasElement.removeClass('no-transition');
-        }, 50);
+            // 다음 프레임에서 no-transition 클래스 제거 (이후 애니메이션 활성화)
+            setTimeout(function() {
+                offcanvasElement.removeClass('no-transition');
+            }, 50);
+        }
+    }
+}
+
+/**
+ * 화면 크기 변경 시 Offcanvas 표시 상태 제어 함수
+ * 1200px 이상에서는 항상 열린 상태를 유지하고
+ * 1200px 미만에서는 닫힌 상태로 변경합니다
+ */
+function handleOffcanvasResize() {
+    const offcanvasElement = $('#offcanvasGnb');
+    const windowWidth = $(window).width();
+
+    if (offcanvasElement.length) {
+        if (windowWidth >= 1200) {
+            // 1200px 이상: 항상 열린 상태 유지
+            if (!offcanvasElement.hasClass('show')) {
+                offcanvasElement.addClass('no-transition');
+                offcanvasElement.addClass('show');
+                setTimeout(function() {
+                    offcanvasElement.removeClass('no-transition');
+                }, 50);
+            }
+        } else {
+            // 1200px 미만: offcanvas 닫기
+            if (offcanvasElement.hasClass('show')) {
+                offcanvasElement.removeClass('show');
+            }
+        }
     }
 }
